4a98e49319ab8eb3ae0c3bc648789d213b150079
dhwoo <dhwoo@linecorp.com>
2023-12-18 18:36:09 +0900

    test google login

--- app/src/main/java/com/place/pani/frame/MainActivity.kt
+++ app/src/main/java/com/place/pani/frame/MainActivity.kt
@@ -1,18 +1,32 @@
 package com.place.pani.frame
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.place.pani.frame.ui.theme.TheFrameTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var singInActivityResultLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,21 +40,57 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        singInActivityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ) { result ->
+            Log.d("Frame", "onActivityResult: ${result.resultCode}")
+            Log.d("Frame", "onActivityResult: ${result.data}")
+//            if (result.resultCode == Activity.RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            handleSignInResult(task)
+//            }
+        }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun signIn() {
+        singInActivityResultLauncher.launch(googleSignInClient.signInIntent)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TheFrameTheme {
-        Greeting("Android")
+    private fun handleSignInResult(task: Task<GoogleSignInAccount>) {
+        try {
+            val account = task.getResult(ApiException::class.java)
+            // Signed in successfully, show authenticated UI.
+            Log.d("Frame", "handleSignInResult: ${account?.displayName}")
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.d("Frame", "signInResult:failed code=${e.statusCode}")
+        }
+    }
+
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+
+        Button(onClick = { signIn() }) {
+            Text("Sign in with Google")
+        }
+
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        TheFrameTheme {
+            Greeting("Android")
+        }
     }
 }
